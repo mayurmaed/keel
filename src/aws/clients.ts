b9623e315e45f3df0c9232b581f5d4bb2cb70d35
Mayur Maed <6448126+mayurmaed@users.noreply.github.com>
2026-07-08 14:51:01 +0530

    feat: aws sdk deps, global config, client factory

---
+++ src/aws/clients.ts
@@ -0,0 +1,35 @@
+import { CloudFormationClient } from "@aws-sdk/client-cloudformation";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { SSMClient } from "@aws-sdk/client-ssm";
+import { CodeBuildClient } from "@aws-sdk/client-codebuild";
+import { ECSClient } from "@aws-sdk/client-ecs";
+import { EC2Client } from "@aws-sdk/client-ec2";
+import { CloudWatchLogsClient } from "@aws-sdk/client-cloudwatch-logs";
+import { STSClient } from "@aws-sdk/client-sts";
+
+export interface AwsClients {
+  cfn: CloudFormationClient;
+  ddb: DynamoDBDocumentClient;
+  ssm: SSMClient;
+  codebuild: CodeBuildClient;
+  ecs: ECSClient;
+  ec2: EC2Client;
+  logs: CloudWatchLogsClient;
+  sts: STSClient;
+}
+
+export function makeClients(region: string): AwsClients {
+  return {
+    cfn: new CloudFormationClient({ region }),
+    ddb: DynamoDBDocumentClient.from(new DynamoDBClient({ region }), {
+      marshallOptions: { removeUndefinedValues: true },
+    }),
+    ssm: new SSMClient({ region }),
+    codebuild: new CodeBuildClient({ region }),
+    ecs: new ECSClient({ region }),
+    ec2: new EC2Client({ region }),
+    logs: new CloudWatchLogsClient({ region }),
+    sts: new STSClient({ region }),
+  };
+}
